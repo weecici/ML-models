dca7cffa3488c0f4180a06b67c9ed4ace8a3d6bb
WiCiCi0310 <cc03102005@gmail.com>
2024-10-14 16:04:15 +0700

    modified linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -4,26 +4,26 @@ from icecream.icecream import ic
 
 class LinearRegression:
 
-    def __init__(self, learning_rate=0.001, n_iters=1000):
+    def __init__(self, learning_rate=0.001, max_iter=1000):
         self.eta = learning_rate
-        self.n_iters = n_iters
-        self.weights = None
-        self.bias = None
+        self.max_iter = max_iter
+        self.w = None
+        self.b = None
 
     def fit(self, X, y):
 
         n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
-        self.bias = 0
+        self.w = np.zeros(n_features)
+        self.b = 0
 
         ic(X.shape)
-        for _ in range(self.n_iters):
-            y_predicted = np.dot(X, self.weights) + self.bias
+        for _ in range(self.max_iter):
+            y_predicted = np.dot(X, self.w) + self.b
 
-            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
-            db = (1 / n_samples) * np.sum(y_predicted - y)
-            self.weights = self.eta * dw
-            self.bias -= self.eta * db
+            gradient_w = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            gradient_b = (1 / n_samples) * np.sum(y_predicted - y)
+            self.w = self.eta * gradient_w
+            self.b -= self.eta * gradient_b
 
     def predict(self, X):
-        return np.dot(X, self.weights) + self.bias
+        return np.dot(X, self.w) + self.b
