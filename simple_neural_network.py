cfc243b9562ab46852ec4c274bb853a1cfa51b3b
WiCiCi0310 <cc03102005@gmail.com>
2024-11-25 20:35:55 +0700

    update NN

--- simple_neural_network.py
+++ simple_neural_network.py
@@ -9,14 +9,25 @@ from scipy import sparse
 matplotlib.use("TKagg")
 
 
+def convert_labels(y, C=3):
+    Y = sparse.coo_matrix(
+        (np.ones_like(y), (y, np.arange(len(y)))), shape=(C, len(y))
+    ).toarray()
+    return Y
+
+
 def softmax(V):
-    e_V = np.exp(V - np.max(V, axis=1, keepdims=True))
-    Z = e_V / e_V.sum(axis=1)[:, np.newaxis]
+    e_V = np.exp(V - np.max(V, axis=0, keepdims=True))
+    Z = e_V / e_V.sum(axis=0)
     return Z
 
 
+def cost(Y, Yhat):
+    return -np.sum(Y * np.log(Yhat)) / Y.shape[1]
+
+
 class NeuralNetwork:
-    def __init__(self, n_neurons_in_hidden_layers: list[int], learning_rate=1e-2):
+    def __init__(self, n_neurons_in_hidden_layers: list[int], learning_rate=1e-4):
         self.eta = learning_rate
         self.n_neurons = [0, *n_neurons_in_hidden_layers, 0]
         self.n_layers = len(self.n_neurons) - 1
@@ -24,26 +35,21 @@ class NeuralNetwork:
         self.B = None
 
     def fit(self, X, y):
-        y = np.eye(len(np.unique(y)))[y]
+        y = convert_labels(y)
 
-        n_samples = X.shape[0]
-        self.n_neurons[0] = X.shape[1]
-        self.n_neurons[-1] = y.shape[1]
+        self.n_neurons[0] = X.shape[0]
+        self.n_neurons[-1] = y.shape[0]
+        N = X.shape[1]
 
         self.W = [
-            np.random.uniform(
-                -0.1, 0.1, size=(self.n_neurons[i], self.n_neurons[i + 1])
-            )
+            np.random.uniform(-1, 1, size=(self.n_neurons[i], self.n_neurons[i + 1]))
             for i in range(self.n_layers)
         ]
 
-        self.B = [
-            np.random.uniform(-0.1, 0.1, size=(1, self.n_neurons[i]))
-            for i in range(1, self.n_layers + 1)
-        ]
+        self.B = [np.zeros((self.n_neurons[i], 1)) for i in range(1, self.n_layers + 1)]
 
-        # SGD
-        for i in range(100):
+        # GD
+        for i in range(10001):
 
             # feedforward
             Z = []
@@ -51,44 +57,48 @@ class NeuralNetwork:
 
             y_hat = X
             for j in range(self.n_layers):
-                y_hat = np.dot(y_hat, self.W[j]) + self.B[j]
+                y_hat = np.dot(self.W[j].T, y_hat) + self.B[j]
                 Z.append(y_hat)
                 if j != self.n_layers - 1:
                     y_hat = np.maximum(y_hat, 0)
                     A.append(y_hat)
 
-            # ic(y_hat)
             y_hat = softmax(y_hat)
 
-            gradient_Bi = y_hat - y
-            gradient_Wi = np.dot(A[-1].T, gradient_Bi)
+            E = (y_hat - y) / N
+            if i % 1000 == 0:
+                ic(i)
+                ic(np.argmax(y_hat, axis=0))
+                ic(cost(y, y_hat))
+            gradient_Wi = np.dot(A[-1], E.T)
 
             self.W[-1] -= self.eta * gradient_Wi
-            self.B[-1] -= self.eta * np.sum(gradient_Bi, axis=0)
+            self.B[-1] -= self.eta * np.sum(E, axis=1, keepdims=True)
 
             for j in range(self.n_layers - 2, -1, -1):
-                gradient_Bi = np.dot(gradient_Bi, self.W[j + 1].T)
-                gradient_Bi[gradient_Bi < 0] = 0
-                gradient_Wi = np.dot(A[j].T, gradient_Bi)
+                E = np.dot(self.W[j + 1], E)
+                E[Z[j] <= 0] = 0
+                gradient_Wi = np.dot(A[j], E.T)
 
                 self.W[j] -= self.eta * gradient_Wi
-                self.B[j] -= self.eta * np.sum(gradient_Bi, axis=0)
+                self.B[j] -= self.eta * np.sum(E, axis=1, keepdims=True)
 
     def predict(self, X):
-        y_pred = X
-        for i in range(self.n_layers):
-            y_pred = np.dot(y_pred, self.W[i]) + self.B[i]
+        y_hat = X
+        for j in range(self.n_layers):
+            y_hat = np.dot(self.W[j].T, y_hat) + self.B[j]
             if j != self.n_layers - 1:
-                y_pred = np.maximum(y_pred, 0)
-        y_pred = softmax(y_pred)
-        return np.argmax(y_pred, axis=1)
+                y_hat = np.maximum(y_hat, 0)
+
+        y_hat = softmax(y_hat)
+        return np.argmax(y_hat, axis=0)
 
 
 N = 10  # number of points per class
 d0 = 2  # dimensionality
 C = 3  # number of classes
 X = np.zeros((d0, N * C))  # data matrix (each row = single example)
-y = np.zeros(N * C, dtype="int")  # class labels
+y = np.zeros(N * C, dtype="uint8")  # class labels
 
 for j in range(C):
     ix = range(N * j, N * (j + 1))
@@ -97,11 +107,20 @@ for j in range(C):
     X[:, ix] = np.c_[r * np.sin(t), r * np.cos(t)].T
     y[ix] = j
 
-
-test = NeuralNetwork([10])
-ic(X)
-test.fit(X.T, y)
-ic(X)
-y_pred = test.predict(X.T)
+test = NeuralNetwork([100, 50, 10])
+test.fit(X, y)
+y_pred = test.predict(X)
 acc = np.sum(y_pred == y) / len(y)
+
+# plt.plot(X[:N, 0], X[:N, 1], "bs", markersize=7)
+# plt.plot(X[N : 2 * N, 0], X[N : 2 * N, 1], "ro", markersize=7)
+# plt.plot(X[2 * N :, 0], X[2 * N :, 1], "g^", markersize=7)
+# plt.xlim([-1.5, 1.5])
+# plt.ylim([-1.5, 1.5])
+# cur_axes = plt.gca()
+# cur_axes.axes.get_xaxis().set_ticks([])
+# cur_axes.axes.get_yaxis().set_ticks([])
+# plt.show()
+ic(y_pred)
+ic(y)
 ic(acc)
